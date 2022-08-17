fbb15b163d77b38beff9828728d5c91b6d99533e
Mohammadsaifuddin <82999564+Mohammadsaifuddin@users.noreply.github.com>
2022-08-17 12:32:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Restaurant from "./components/basics/Restaurant";
+import Todo from "./components/todoreact/Todo";
+
+const App = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
